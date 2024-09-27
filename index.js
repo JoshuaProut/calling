a7130e426b7967a234e5d944ac3946982e4783e7
Josh Prout <jnp207@exeter.ac.uk>
2024-09-27 15:25:39 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+import { CallClient } from "@azure/communication-calling";
+import { AzureCommunicationTokenCredential } from '@azure/communication-common';
+
+let call;
+let incomingCall;
+let callAgent;
+let deviceManager;
+let tokenCredential;
+const userToken = document.getElementById("token-input"); 
+const calleeInput = document.getElementById("callee-id-input");
+const submitToken = document.getElementById("token-submit");
+const callButton = document.getElementById("call-button");
+const hangUpButton = document.getElementById("hang-up-button");
+const acceptCallButton = document.getElementById('accept-call-button');
